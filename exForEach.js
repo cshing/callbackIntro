aff1354d8c07995bc538a6447da88192953fbe5b
Carmen Shing <carmenshing@gmail.com>
2018-06-07 19:37:46 +0000

    Used forEach instead of for loop

---
+++ exForEach.js
@@ -0,0 +1,35 @@
+//ORIGINAL CODE
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found();   // execute callback
+      }
+    }
+  }
+  
+  function actionWhenFound() {
+    console.log("Found him!");
+  }
+  
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+//EXERCISE - Array forEach
+
+function findWaldo(arr, found) {
+    arr.forEach(function(nameInArray) {
+
+        if (nameInArray === "Waldo") {
+            found();   // execute callback
+        }
+      });
+    }
+    
+  function actionWhenFound() {
+    console.log("Found him!");
+  }
+  
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
